591b4c47ab11fcad58ea729a8f25e7ad8ffa18cf
RaulMolinaC <72096156+RaulMolinaC@users.noreply.github.com>
2020-10-19 17:32:15 +0200

    MainMenu
    Print Main Funcion

---
+++ CalculadoraMadeInChina/CalculadoraMadeInChina/UserInterface.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CalculadoraMadeInChina
+{
+    class UserInterface
+    {
+        public static void PrintMainMenu()
+        {
+
+            System.Console.WriteLine(" Calculadora Mde in China ");
+            System.Console.WriteLine(" 1) Sumar una serie de números ");
+            System.Console.WriteLine(" 2) Restar una serie de números ");
+            System.Console.WriteLine(" 3) Multiplicar ");
+            System.Console.WriteLine(" 4) Dividir ");
+            System.Console.WriteLine(" 5) Raices ");
+            System.Console.WriteLine(" 6) Números primos ");
+            System.Console.WriteLine(" 7) Sumatorios ");
+            System.Console.WriteLine(" 0) Salir de la aplicación ");
+
+
+
+
+
+        }
+    }
+}
